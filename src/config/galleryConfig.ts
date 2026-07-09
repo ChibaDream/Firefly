b26f5a7eb9812f0aba164b1474c27fe8dc777321
千叶梦奈 <3076265118@qq.com>
2026-07-09 17:44:21 +0800

    add gallary

--- src/config/galleryConfig.ts
+++ src/config/galleryConfig.ts
@@ -15,7 +15,7 @@ export const galleryConfig: GalleryConfig = {
 		// password: 访问密码，设置后需要输入密码才能查看相册内容（可选）
 		// passwordHint: 密码提示，设置后在输入密码错误时显示（可选，需配合password使用）
 		// 每添加一个数组项就相当于添加了一个相册，记得在 public/gallery/ 目录下创建对应的子目录并放入图片
-		{
+		/*{
 			id: "firefly-2026",
 			name: "可爱流萤",
 			description: "飞萤之火自无梦的长夜亮起，绽放在终竟的明天。",
@@ -33,7 +33,24 @@ export const galleryConfig: GalleryConfig = {
 			tags: ["加密相册", "示例"],
 			password: "123456",
 			passwordHint: "示例密码123456",
+		},*/
+		{
+			id:"landscape-painting",
+			name:"风景创作",
+			description:"绘画记录",
+			location:"地球",
+			date:"2026-07-09",
+			tags:["绘画作品"]
 		},
+		{
+			id:"rossi",
+			name:"洛茜",
+			description:"The name of the Pack will echo across the land!",
+			location:"塔卫二",
+			date:"2026-07-09",
+			tags:["明日方舟：终末地","洛茜"]
+		},
+
 	],
 
 	// 瀑布流最小列宽(px)，浏览器根据容器宽度自动计算列数，默认 240
